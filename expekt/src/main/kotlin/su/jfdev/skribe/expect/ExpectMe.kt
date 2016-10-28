a3960664717882c74504be3a8b4ecb000a2096a6
Jamefrus <jamefrus@ya.ru>
2016-10-28 09:35:17 +0300

    Add get properties for default expects and fixed ExpectMe return type

--- expekt/src/main/kotlin/su/jfdev/skribe/expect/ExpectMe.kt
+++ expekt/src/main/kotlin/su/jfdev/skribe/expect/ExpectMe.kt
@@ -3,29 +3,29 @@ package su.jfdev.skribe.expect
 import com.winterbe.expekt.*
 
 abstract class ExpectMe<T, ME: ExpectMe<T, ME>>(subject: T?, flavor: Flavor): ExpectAny<T>(subject, flavor) {
-    override val a: ExpectAny<T> get() = me { super.a }
-    override val an: ExpectAny<T> get() = me { super.an }
-    override val and: ExpectAny<T> get() = me { super.and }
-    override val at: ExpectAny<T> get() = me { super.at }
-    override val be: ExpectAny<T> get() = me { super.be }
-    override val been: ExpectAny<T> get() = me { super.been }
-    override val has: ExpectAny<T> get() = me { super.has }
-    override val have: ExpectAny<T> get() = me { super.have }
-    override val `is`: ExpectAny<T> get() = me { super.`is` }
-    override val not: ExpectAny<T> get() = me { super.not }
-    override val `null`: ExpectAny<T> get() = me { super.`null` }
-    override val of: ExpectAny<T> get() = me { super.of }
-    override val same: ExpectAny<T> get() = me { super.same }
-    override val that: ExpectAny<T> get() = me { super.that }
-    override val the: ExpectAny<T> get() = me { super.the }
-    override val to: ExpectAny<T> get() = me { super.to }
-    override val which: ExpectAny<T> get() = me { super.which }
-    override val with: ExpectAny<T> get() = me { super.with }
+    override val a: ME get() = me { super.a }
+    override val an: ME get() = me { super.an }
+    override val and: ME get() = me { super.and }
+    override val at: ME get() = me { super.at }
+    override val be: ME get() = me { super.be }
+    override val been: ME get() = me { super.been }
+    override val has: ME get() = me { super.has }
+    override val have: ME get() = me { super.have }
+    override val `is`: ME get() = me { super.`is` }
+    override val not: ME get() = me { super.not }
+    override val `null`: ME get() = me { super.`null` }
+    override val of: ME get() = me { super.of }
+    override val same: ME get() = me { super.same }
+    override val that: ME get() = me { super.that }
+    override val the: ME get() = me { super.the }
+    override val to: ME get() = me { super.to }
+    override val which: ME get() = me { super.which }
+    override val with: ME get() = me { super.with }
 
-    override fun equal(expected: T?): ExpectAny<T> = me { super.equal(expected) }
-    override fun identity(expected: T?): ExpectAny<T> = me { super.identity(expected) }
-    override fun <S: T> instanceof(type: Class<S>): ExpectAny<T> = me { super.instanceof(type) }
-    override fun satisfy(predicate: (T) -> Boolean): ExpectAny<T> = me { super.satisfy(predicate) }
+    override fun equal(expected: T?): ME = me { super.equal(expected) }
+    override fun identity(expected: T?): ME = me { super.identity(expected) }
+    override fun <S: T> instanceof(type: Class<S>): ME = me { super.instanceof(type) }
+    override fun satisfy(predicate: (T) -> Boolean): ME = me { super.satisfy(predicate) }
 
     @Suppress("UNCHECKED_CAST")
     protected inline fun me(action: () -> Unit) = apply { action() } as ME
--- expekt/src/main/kotlin/su/jfdev/skribe/expect/ExpectSomething.kt
+++ expekt/src/main/kotlin/su/jfdev/skribe/expect/ExpectSomething.kt
@@ -20,6 +20,31 @@ fun <T> ExpectAny<T>.get(property: KProperty1<T, Duration?>) = UnsafeExpect.run 
     child(property) { ExpectDuration(it, EXPECT) }
 }
 
+@JvmName("booleanProperty")
+fun <T> ExpectAny<T>.get(property: KProperty1<T, Boolean?>) = UnsafeExpect.run {
+    child(property) { ExpectBoolean(it, EXPECT) }
+}
+
+@JvmName("doubleProperty")
+fun <T> ExpectAny<T>.get(property: KProperty1<T, Double?>) = UnsafeExpect.run {
+    child(property) { ExpectDouble(it, EXPECT) }
+}
+
+@JvmName("stringProperty")
+fun <T> ExpectAny<T>.get(property: KProperty1<T, String?>) = UnsafeExpect.run {
+    child(property) { ExpectString(it, EXPECT) }
+}
+
+@JvmName("collectionProperty")
+fun <T, R> ExpectAny<T>.get(property: KProperty1<T, Collection<R>?>) = UnsafeExpect.run {
+    child(property) { ExpectCollection(it, EXPECT) }
+}
+
+@JvmName("mapProperty")
+fun <T, K, V> ExpectAny<T>.get(property: KProperty1<T, Map<K, V>?>) = UnsafeExpect.run {
+    child(property) { ExpectMap(it, EXPECT) }
+}
+
 @JvmName("anyFunction")
 fun <T, R> ExpectAny<T>.get(property: KFunction1<T, R?>) = UnsafeExpect.run {
     child(property) { ExpectAny(it, EXPECT) }
@@ -33,5 +58,30 @@ fun <T, R: Comparable<R>> ExpectAny<T>.get(property: KFunction1<T, R?>) = Unsafe
 @JvmName("durationFunction")
 fun <T> ExpectAny<T>.get(property: KFunction1<T, Duration?>) = UnsafeExpect.run {
     child(property) { ExpectDuration(it, EXPECT) }
+}
+
+@JvmName("booleanFunction")
+fun <T> ExpectAny<T>.get(property: KFunction1<T, Boolean?>) = UnsafeExpect.run {
+    child(property) { ExpectBoolean(it, EXPECT) }
+}
+
+@JvmName("doubleFunction")
+fun <T> ExpectAny<T>.get(property: KFunction1<T, Double?>) = UnsafeExpect.run {
+    child(property) { ExpectDouble(it, EXPECT) }
+}
+
+@JvmName("stringFunction")
+fun <T> ExpectAny<T>.get(property: KFunction1<T, String?>) = UnsafeExpect.run {
+    child(property) { ExpectString(it, EXPECT) }
+}
+
+@JvmName("collectionFunction")
+fun <T, R> ExpectAny<T>.get(property: KFunction1<T, Collection<R>?>) = UnsafeExpect.run {
+    child(property) { ExpectCollection(it, EXPECT) }
+}
+
+@JvmName("mapFunction")
+fun <T, K, V> ExpectAny<T>.get(property: KFunction1<T, Map<K, V>?>) = UnsafeExpect.run {
+    child(property) { ExpectMap(it, EXPECT) }
 }
 
