0b4bd2edc54c3ef0c1868927a1fb85f5e6ebd13a
Jamefrus <jamefrus@ya.ru>
2016-12-14 10:19:03 +0200

    SuiteAdapter implements Testable

--- skribe/src/main/kotlin/com/github/jnexil/skribe/adapter/SuiteAdapter.kt
+++ skribe/src/main/kotlin/com/github/jnexil/skribe/adapter/SuiteAdapter.kt
@@ -1,9 +1,12 @@
 package com.github.jnexil.skribe.adapter
 
-interface SuiteAdapter {
+import com.github.jnexil.skribe.testable.*
+
+interface SuiteAdapter: Testable<Unit> {
     fun beforeEach(action: () -> Unit)
     fun afterEach(action: () -> Unit)
     fun suite(name: String, action: SuiteAdapter.() -> Unit): SuiteAdapter
     fun suite(name: String): SuiteAdapter
     fun case(name: String, test: () -> Unit): CaseAdapter
+    override fun test(description: String, action: (Unit) -> Unit): CaseAdapter = case(description) {}
 }
