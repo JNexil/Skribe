762bf85bbb4c17531066cd18d30a47d5eaa331ed
Jamefrus <jamefrus@ya.ru>
2016-12-17 20:28:03 +0200

    v0.3.2: add logging for intermediate and stepwise

--- skribe/src/main/kotlin/com/github/jnexil/skribe/testable/AdaptIntermediate.kt
+++ skribe/src/main/kotlin/com/github/jnexil/skribe/testable/AdaptIntermediate.kt
@@ -1,14 +1,42 @@
 package com.github.jnexil.skribe.testable
 
 import com.github.jnexil.skribe.adapter.*
+import mu.*
 
 internal class AdaptIntermediate<out Subject>(val suite: SuiteAdapter, val factory: () -> Subject): Intermediate<Subject> {
     override fun test(description: String, action: (Subject) -> Unit) = suite.case(description) {
-        factory().run(action)
+        doAction(description, action)
     }
 
     override fun <R> move(description: String, action: (Subject) -> R): Intermediate<R> = AdaptIntermediate(suite = suite.suite(description)) {
-        factory().run(action)
+        doAction(description, action)
+    }
+
+    private inline fun <R> doAction(description: String, action: (Subject) -> R): R {
+        val subject = factory()
+        val mapped = action(subject)
+        logger.info {
+            buildString {
+                append('#')
+                append(' ')
+                append(description)
+                append('\t')
+                append('#')
+                append(subject.name())
+                append(" -> ")
+                when (subject) {
+                    mapped -> append('|')
+                    else   -> append(mapped.name())
+                }
+            }
+        }
+        return mapped
+    }
+
+    private fun Any?.name(): String = when (this) {
+        is Unit -> "Unit"
+        null    -> "null"
+        else    -> this.javaClass.simpleName
     }
 
     override fun share(description: String): Intermediate<Subject> {
@@ -18,4 +46,6 @@ internal class AdaptIntermediate<out Subject>(val suite: SuiteAdapter, val facto
     override fun <R> move(action: (Subject) -> R): Intermediate<R> = AdaptIntermediate(suite) {
         factory().run(action)
     }
+
+    companion object: NamedKLogging(name = Intermediate::class.java.name)
 }
